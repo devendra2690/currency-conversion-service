09d19f25154727e448dbdea5a22f7258fae25e98
devendra2690 <devendra_patil26@yahoo.com>
2019-10-19 15:58:19 +0530

    Ribbon support to application

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxyService.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxyService.java
@@ -1,13 +1,16 @@
 package com.in28minutes.microservices.currencyconversionservice;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name="currency-conversion-service",url="localhost:8000")
+//@FeignClient(name="currency-conversion-service",url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
+@RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeProxyService {
 
-	@GetMapping("/currency-exchnage/{from}/to/{to}")
+	@GetMapping("/currency-exchange/{from}/to/{to}")
 	public CurrencyConversion getExChangeRate(@PathVariable("from") String from, @PathVariable("to") String to);
 }
